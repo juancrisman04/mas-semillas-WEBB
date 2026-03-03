8d3f572cb92085bdee7cc0069f874d4123353302
juancrisman04 <juancrisman04@gmail.com>
2026-03-03 18:00:40 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./pages/**/*.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'system-ui', '-apple-system', 'sans-serif'],
+      },
+      colors: {
+        red: {
+          600: '#F1C400',
+          700: '#D2D755',
+        },
+        zinc: {
+          950: '#228848',
+        },
+      },
+      animation: {
+        'float': 'float 6s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
